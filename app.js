c676bb10db6dc00321742eceb23297788511842b
mliay81 <mikeliay@gmail.com>
2019-01-03 21:09:07 -0600

    setup

---
+++ app.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    console.log( "ready!" )
+
+var express = require("express")
+var app = express()
+app.set("view engine", "ejs")
+
+app.listen(3000)
+
+app.get('/', function(req, res) {
+    res.render('home')
+  })
+
+app.get('/about', function(req, res) {
+    res.render('about')
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
